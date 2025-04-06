a9281c1de1f8595eb86f2d91a874345f5142cc9e
LincThai <78945845+LincThai@users.noreply.github.com>
2025-04-07 09:07:08 +1000

    Movement wasd complete unsmothed  angle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,22 @@ public class PlayerController : MonoBehaviour
     {
         // get input values
         float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        float z = Input.GetAxisRaw("Vertical"); // also known as y in 2d graph
+
+        // set a nomalized vector for movement based on input
+        Vector3 direction = new Vector3(x, 0, z).normalized;
+
+        // check their has been input
+        if (direction.magnitude >= 0.1f)
+        {
+            // find the target angle based on movement and apply
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+
+            // apply movement
+            controller.Move(direction * walkSpeed * Time.deltaTime);
+        }
+
+
     }
 }
