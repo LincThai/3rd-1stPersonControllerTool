d83f28f1c591582536848a800ae95c5ddbe20197
LincThai <78945845+LincThai@users.noreply.github.com>
2025-04-19 17:04:23 +1000

    Add Crouch Mechanic
    Untested, No Mesh Change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // set variable
     // references
+    [Header("References")]
     public CharacterController controller;
     public Transform cam;
 
     // basic player variables
+    [Header("Basic Player Movement Info")]
+    public float crouchSpeed = 2.5f;
     public float walkSpeed = 5;
     public float runSpeed = 10;
     public float jumpHeight = 2;
@@ -18,32 +22,65 @@ public class PlayerController : MonoBehaviour
     float moveSpeed;
 
     // angle smoothing
+    [Header("Player rotation smoothing")]
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     // jump/grounding variables
+    [Header("Jump/Grounding Check")]
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     bool isGrounded;
     Vector3 velocity;
 
+    // Crouch Variables
+    bool isCrouching = false;
+    public float crouchHeight = 1;
+    public float walkHeight = 2;
+    float currentHeight;
+
     private void Start()
     {
         // set a speed of movement to the walking speed
         moveSpeed = walkSpeed;
+        // set current height of collider and regular height of walking/running
+        currentHeight = controller.height;
+        walkHeight = currentHeight;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // walk and run
+        // walk, run and crouch
+        if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+        }
+
         // get input values
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical"); // also known as y in 2d graph
 
         // set a nomalized vector for movement based on input
         Vector3 direction = new Vector3(x, 0, z).normalized;
+
+        if (isCrouching == true)
+        {
+            // when crouching
+            // lerp between the current speed toward the crouching speed by gradually decreasing by the acceleration over time
+            moveSpeed = Mathf.Lerp(moveSpeed, crouchSpeed, acceleration * Time.deltaTime);
+            // change character controller collider height smoothly to crouch height
+            currentHeight = Mathf.Lerp(currentHeight, crouchHeight, acceleration * Time.deltaTime);
+        }
+        else 
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+            // change character controller collider height smoothly to walk height
+            currentHeight = Mathf.Lerp(currentHeight, walkHeight, acceleration * Time.deltaTime);
+        }
+        // apply height changes
+        controller.height = currentHeight;
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -54,11 +91,10 @@ public class PlayerController : MonoBehaviour
         else
         {
             // when sprint key is released
-            // lerp between the current speed of movement to the walking speed by increasing gradually by the acceleration over time
+            // lerp between the current speed of movement to the walking speed by increasing/decreasing
+            // gradually by the acceleration over time
             moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
         }
-
-        Debug.Log(moveSpeed);
 
         // check their has been input
         if (direction.magnitude >= 0.1f)
